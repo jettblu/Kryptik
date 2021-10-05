52c1e3b0389a83737ff1696b4ad843c8f68c07b0
jettblu <jettblu.fabrications@gmail.com>
2021-10-05 19:09:20 -0400

    Add internet connection handling

--- CrypticPay/wwwroot/js/site.js
+++ CrypticPay/wwwroot/js/site.js
@@ -3,4 +3,24 @@
 
 // Write your Javascript code.
 
+window.addEventListener('offline', function () {
+    var pageAll = document.getElementById("allContent"); 
+    var tag = document.createElement("h4");
+    var text = document.createTextNode("Sorry, Kryptik App requires an internet connection to work.");
+    tag.setAttribute("id", "connectionStatus");
+    tag.appendChild(text);
+    pageAll.appendChild(tag);
+    var content = document.getElementById("pageContent"); 
+    content.style.display = "none";;
+});
 
+window.addEventListener('online', function () {
+    var statusTag = document.getElementById("connectionStatus");
+    if (statusTag == null) {
+        // do nothing
+    }
+    else {
+        // reshow page content
+        location.reload();
+    }
+});
