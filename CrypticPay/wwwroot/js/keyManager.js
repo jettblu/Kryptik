703ce559a3682ffd703ff35058ab9bf46d7c3fd9
jettblu <jettblu.fabrications@gmail.com>
2021-10-14 15:21:30 -0400

    Add client side seed and xpub genration

--- CrypticPay/wwwroot/js/keyManager.js
+++ CrypticPay/wwwroot/js/keyManager.js
@@ -1,15 +1,19 @@
 ﻿// creates shares for a given secret
 // returns xpub and 1 seed share for remote storage
 // one seed share is saved on client
-function createShares() {
-    var mnemonic = generateMnemonic(wordlists.english);
-    // retrieve seed from mnemonic to be used
-    var seed = mnemonicToSeed(mnemonic);
-    var hdkey = fromMasterSeed(Buffer.from(seed, 'hex'));
-    var extPubKey = hdkey.publicExtendedKey();
-    // convert the seed into a hex string
-    var seedHex = secrets.str2hex(seed);
-    var shares = secrets.share(seedHex, 4, 2);
+var createShares = function () {
+    console.log("Creating shares");
+    var mnemonic = bip.generateMnemonic();
+    console.log(mnemonic);
+    // retrieve seed from mnemonic to be used for xpub generation
+    var seed = bip.mnemonicToSeed(mnemonic);
+    var hdk = hdkey.fromMasterSeed(seed);
+    // get extended key for remote storage
+    var extPubKey = hdk.publicExtendedKey;
+    // convert mnemonic to hex
+    var mnemonicHex = shamir.str2hex(mnemonic);
+    var shares = shamir.share(mnemonicHex, 4, 2);
+    console.log(shares);
     // save one share to browser's local memory
     localStorage.setItem("seedShare", shares[0]);
     // use another share for remote storage
@@ -20,5 +24,19 @@ function createShares() {
         xpub: extPubKey
     };
     console.log("Shares created.");
+    var mnemonicRes = combineShares(localStorage.getItem("seedShare"), remoteShare);
+    console.log(mnemonicRes);
     return remoteData;
 }
+
+
+// combines shares to regenerate seed
+var combineShares = function (share1, share2) {
+    // combine 2 shares
+    comb = shamir.combine([share1, share2])
+    //convert Hex back to regular string
+    res = shamir.hex2str(comb)
+    return res;
+}
+
+
---
+++ CrypticPay/wwwroot/js/test.js
@@ -0,0 +1,10 @@
+﻿// script used with community/test for testing purposes
+
+
+var runSim = function () {
+    console.log("Sim started!");
+    var mnem = bip.generateMnemonic();
+    console.log(mnem);
+}
+
+$("#testBtn").on('click', createShares);
