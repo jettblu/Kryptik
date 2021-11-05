910f6acebd351abeeb466482336e0d1f3c65577b
jettblu <jettblu.fabrications@gmail.com>
2021-11-05 16:19:01 -0400

    Update style for form input and message transitions

--- CrypticPay/wwwroot/js/chatHubManager.js
+++ CrypticPay/wwwroot/js/chatHubManager.js
@@ -1,3 +1,12 @@
 ﻿$(".msgSideClose").on('click', function () {
     $("#slideOut").sideNav('hide');
 });
+
+/*show contact search box on request*/
+$(".msgNew").on('click', function () {
+    $("#slideOut").sideNav('hide');
+    $("#msgCreateArea").hide('slow');
+    $("#msgHistoryArea").hide('slow');
+    $("#msgBlankArea").hide('slow');
+    $("#msgSearchArea").show('fast');
+});
