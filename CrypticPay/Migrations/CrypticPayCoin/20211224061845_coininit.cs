53a51a4866c3b0015f2293c8b6467ea5c26c105b
jettblu <jettblu.fabrications@gmail.com>
2021-12-23 22:20:59 -0800

    Fix context migrations

---
+++ CrypticPay/Migrations/CrypticPayCoin/20211224061845_coininit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrypticPay.Migrations.CrypticPayCoin
+{
+    public partial class coininit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ CrypticPay/Migrations/CrypticPayFriendship/20211224061940_friendcontext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrypticPay.Migrations.CrypticPayFriendship
+{
+    public partial class friendcontext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
